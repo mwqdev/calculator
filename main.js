9d448932e8756bb22168f8f2d511416699ece391
mwqdev <quarles.michael@gmail.com>
2017-06-10 16:57:22 -0400

    Basic functionality. Should not allow multiple operators in sequence or for the equation to end/begin with an operator

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
-var clearButton = document.getElementById('clearButton');
-var inputView = document.getElementById('inputView');
-var key1 = document.getElementById('key1');
-var key2 = document.getElementById('key2');
-var key3 = document.getElementById('key3');
-var key4 = document.getElementById('key4');
-var key5 = document.getElementById('key5');
-var key6 = document.getElementById('key6');
-var key7 = document.getElementById('key7');
-var key8 = document.getElementById('key8');
-var key9 = document.getElementById('key9');
-var key0 = document.getElementById('key0');
-var subtractOperator = document.getElementById('subtractOperator');
-var divideOperator = document.getElementById('divideOperator');
-var multiplyOperator = document.getElementById('multiplyOperator');
-var keyDecimal = document.getElementById('keyDecimal');
-var keyEquals = document.getElementById('keyEquals');
-var addOperator = document.getElementById('addOperator');
+var keys = document.querySelectorAll('.key');
 
-var number = document.querySelectorAll('number');
+for(i=0; i < keys.length; i++) {
+    keys[i].addEventListener('click', function() {
+        var input = document.querySelector('.inputView');
+        var inputValue = input.innerHTML;
+        var keyValue = this.innerHTML;
+
+        if(keyValue == 'C') {
+            input.innerHTML = '';
+        } else if (keyValue == '=') {
+            var equation = inputValue;
+            equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
+            input.innerHTML = eval(equation);
+        } else {
+            input.innerHTML += keyValue;
+        }
+    });
+}
