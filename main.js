a88c64306546079e8efb8225414c16eb7219a667
mwqdev <quarles.michael@gmail.com>
2017-06-10 16:58:28 -0400

    commit before master merge

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var keys = document.querySelectorAll('.key');
 
-for(i=0; i < keys.length; i++) {
-    keys[i].addEventListener('click', function() {
+for (i = 0; i < keys.length; i++) {
+    keys[i].addEventListener('click', function () {
         var input = document.querySelector('.inputView');
         var inputValue = input.innerHTML;
         var keyValue = this.innerHTML;
 
-        if(keyValue == 'C') {
+        if (keyValue == 'C') {
             input.innerHTML = '';
         } else if (keyValue == '=') {
             var equation = inputValue;
